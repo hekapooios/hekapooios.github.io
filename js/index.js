b3cb76a84e3d60ebc93002c85117969e9a37ad5b
Hekapoo <52130719+hekapooios@users.noreply.github.com>
2020-07-24 14:14:47 +0300

    Added T8006 SEPROM

--- js/index.js
+++ js/index.js
@@ -69,7 +69,7 @@ function main() {
 
 	var xhr = new XMLHttpRequest();
 
-	xhr.open("GET", "resources/index.json?v=7", true);
+	xhr.open("GET", "resources/index.json?v=8", true);
 	xhr.send();
 	xhr.onreadystatechange = function() {
 
