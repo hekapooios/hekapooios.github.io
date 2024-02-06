535ad446cf967fec1dc78109b987f26bada1d125
Hekapoo <52130719+hekapooios@users.noreply.github.com>
2024-02-06 21:20:44 +0100

    Added S5L8723 & S5L8740 ROMs

--- js/index.js
+++ js/index.js
@@ -64,7 +64,7 @@ function main() {
 
 	var xhr = new XMLHttpRequest();
 
-	xhr.open("GET", "resources/index.json?v=11", true);
+	xhr.open("GET", "resources/index.json?v=12", true);
 	xhr.send();
 	xhr.onreadystatechange = function() {
 
