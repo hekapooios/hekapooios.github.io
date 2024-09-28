9b5bcc2e175d025abeb11b2e3a674d94549d15d9
Hekapoo <52130719+hekapooios@users.noreply.github.com>
2024-09-28 22:05:15 +0200

    Anniversary initial commit

--- js/index.js
+++ js/index.js
@@ -1,43 +1,140 @@
-function fillTab(tab, tabContents) {
-    if (tab.id == "ap-tab") {
-        tabContents.innerHTML = Mustache.render(template, map["aps"]);
-    } else if (tab.id == "sep-tab") {
-        tabContents.innerHTML = Mustache.render(template, map["seps"]);
-    } else if (tab.id == "rtkit-tab") {
-        tabContents.innerHTML = Mustache.render(template, map["rtkits"]);
-    } else if (tab.id == "samsung-tab") {
-        tabContents.innerHTML = Mustache.render(template, map["samsungs"]);
-    }
-
-    tab.inited = 1;
-}
-
-function tabHandler(event) {
-    var requestedTab = event.srcElement;
-    var activeTab = document.getElementById("tabs").getElementsByClassName("tab-active-dark")[0];
-
-    if (requestedTab == activeTab) 
-        return;
-
-    if (activeTab) {
-        activeTab.classList.remove("tab-active-dark");
-        document.getElementById(activeTab.id + "-contents").hidden = true;
-    }
-
-    requestedTab.classList.add("tab-active-dark");
-
-    var requestedTabContents = document.getElementById(requestedTab.id + "-contents");
-    requestedTabContents.hidden = false;
-
-    if (!requestedTab.inited) {
-        fillTab(requestedTab, requestedTabContents);
-    }
-}
-
 var template = undefined;
+var sort = undefined;
+var filter = undefined;
+var query = undefined;
+var __inited = false;
+var __search = undefined;
+
+const __sort_map = {
+    "date" : "__date_sort",
+    "version" : "__version_sort"
+}
+
+function enableSettings(enable) {
+    if (enable) {
+        document.getElementById("sort-container").classList.remove("disabled");
+        document.getElementById("filter-container").classList.remove("disabled");
+    } else {
+        document.getElementById("sort-container").classList.add("disabled");
+        document.getElementById("filter-container").classList.add("disabled");
+    }
+}
+
+function updateView() {
+    if (!__inited) {
+        return;
+    }
+
+    if (query) {
+        enableSettings(false);
+
+        search_results = __search.search(query);
+        results = new Array();
+
+        search_results.forEach(function (value, index) {
+            results.push(value["item"]);
+        })
+
+    } else {
+        enableSettings(true);
+
+        var sort_key = __sort_map[sort];
+        results = map[filter].roms.sort(function (a, b) {
+            return b[sort_key] - a[sort_key];
+        });
+    }
+
+    rendered = new Array();
+
+    results.forEach(function (value, index) {
+        rendered.push(Mustache.render(template, value));
+    });
+
+    contents = document.getElementById("contents");
+
+    if (rendered.length) {
+        contents.innerHTML = rendered.join("<div class=\"divider\" style=\"margin: 20px auto;\"></div>");
+    } else {
+        contents.innerHTML = "<div class=\"notfound\">Nothing found!</div>";
+    }
+}
+
+function segmentControlInit(sortControls, callback) {
+    elements = sortControls.querySelectorAll(".segment-control");
+
+    for (var index = 0; index < elements.length; index++) {
+        const element = elements[index];
+        element.onclick = function (event) {
+            var srcElement = event.srcElement;
+            srcElement.parentElement.querySelector(".segment-control-active").classList.remove("segment-control-active");
+            srcElement.classList.add("segment-control-active");
+            callback(srcElement.id);
+        };
+
+        if (index == 0) {
+            element.classList.add("segment-control-active");
+            callback(element.id);
+        }
+    }
+}
+
+function initSearch() {
+    var map_normalised = new Array();
+
+    for (key in map) {
+        map[key].roms.forEach(function (item, _) {
+            map_normalised.push(item);
+        });
+    };
+
+    __search = new Fuse(map_normalised, {
+        keys : [
+            "name",
+            "version",
+            "from",
+            "desc"
+        ],
+        threshold : 0.3
+    });
+}
+
+function initCount() {
+    var totalLength = 0;
+
+    for (key in map) {
+        totalLength += map[key].roms.length;
+    };
+
+    document.getElementById("count").innerText = totalLength;
+}
+
+function init() {
+    initSearch();
+
+    initCount();
+
+    segmentControlInit(document.getElementById("sort-controls"), function (id) {
+        sort = id;
+        updateView();
+    });
+
+    segmentControlInit(document.getElementById("filter-controls"), function (id) {
+        filter = id;
+        updateView();
+    });
+
+    document.getElementById("search-controls").addEventListener("input", function (event) {
+        var srcElement = event.srcElement;
+        query = srcElement.value.trim();
+        updateView();
+    });
+
+    __inited = true;
+    updateView();
+}
 
 function main() {
-    var templatePath = "templates/rom.html?v=3";
+    var templatePath = "templates/rom.html?v=4";
     var xhr = new XMLHttpRequest();
 
     xhr.open("GET", templatePath, true);
@@ -49,14 +146,7 @@ function main() {
             alert("Wow, this is unexpected, but we really couldn't load the template!");
         } else {
             template = xhr.responseText;
-            
-            var tabs = document.getElementById("tabs").getElementsByClassName("tab");
-
-			for (var i = 0; i < tabs.length; i++) {
-				tabs[i].onclick = tabHandler;
-			}
-
-            tabHandler({srcElement : document.getElementById("ap-tab")});
+            init();
         }
     }
 }
---
+++ map/create_js_map.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+from datetime import datetime
+
+OUT_FILE = "__map.js"
+JS_TEMPLATE = "var map = \n%s"
+TAG_COUNT_MAX = 5
+
+# Not worth the hassle due to 8942 being "rom",
+# half the SEPROMs being privately build and etc.
+
+# def convert_version(version: str) -> int:
+#     splitted = version.split("-")
+#     tag = splitted[1]
+
+#     tag_splitted = tag.split(".")
+
+#     result = 0
+
+#     for i in range(TAG_COUNT_MAX):
+#         if i == 0:
+#             max_count = 5
+#         else:
+#             max_count = 3
+        
+#         try:
+#             result += int(tag_splitted[i]) * \
+#                 (10 ** (TAG_COUNT_MAX - i + max_count))
+#         except IndexError:
+#             break
+
+#     return result
+
+def process_map(map_raw: dict):
+    for key, val in map_raw.items():
+        print("processing %s" % key)
+
+        for idx, entry in enumerate(val["roms"]):
+            date = entry["date"]
+            version = entry["version"]
+
+            timestamp = int(datetime.strptime(date, "%d.%m.%y").timestamp())
+
+            # See the note above            
+            # if version == "rom":
+            #     version = "iBoot-878.8"
+
+            # if key == "aps":
+            #     int_version = convert_version(version)
+            # else:
+            #     int_version = idx
+
+            entry["__date_sort"] = timestamp
+            entry["__version_sort"] = idx   # int_version
+
+def main():
+    if len(sys.argv) != 2:
+        print("%s <in>" % sys.argv[0])
+        print("output will be written to %s" % OUT_FILE)
+        exit(-1)
+
+    _, inp = sys.argv
+
+    with open(inp, "r") as f:
+        map_raw = json.load(f)
+
+    process_map(map_raw)
+
+    with open(OUT_FILE, "w") as f:
+        f.write(JS_TEMPLATE % json.dumps(map_raw, indent=4))
+
+    print("DONE")
+
+if __name__ == "__main__":
+    main()
